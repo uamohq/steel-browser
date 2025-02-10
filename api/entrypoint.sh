8a42571e891fd00c9e242388d9186e236e2acec6
Christopher Mah <github@christophermah.com>
2025-02-10 01:26:52 -0700

    Update

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -34,17 +34,25 @@ dbus-daemon --system --fork
 if ! pgrep -x "Xvfb" > /dev/null
 then
     echo "Starting Xvfb..."
-    Xvfb :10 -screen 0 1920x1080x8 -ac &
+    Xvfb :99 -screen 0 1280x720x24 -ac &
 else
     echo "Xvfb is already running."
 fi
 
 # Set the DISPLAY environment variable
-export DISPLAY=:10
+export DISPLAY=:99
 
 # Set the CDP_REDIRECT_PORT environment variable
 export CDP_REDIRECT_PORT=9223
 export HOST=0.0.0.0
+
+# Start X11VNC (VNC server)
+echo "Starting X11VNC..."
+x11vnc -display :99 -forever -shared -nopw -rfbport 5900 &
+
+# Start noVNC (web-based VNC viewer)
+echo "Starting noVNC..."
+websockify --web=/usr/share/novnc/ 6080 localhost:5900 &
 
 # Run the `npm run start` command but without npm.
 # NPM will introduce its own signal handling
